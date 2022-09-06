369adad77258df7effe4e10398b6096afc48ab8b
shakhboz-abdulazizov <shahboz.abdulazizov@gmail.com>
2022-09-06 00:12:57 -0300

    Initialise project from template repository

--- benthicnet_ssl/__meta__.py
+++ benthicnet_ssl/__meta__.py
@@ -1,12 +1,12 @@
 # `name` is the name of the package as used for `pip install package`
-name = "package-name"
+name = "benthicnet-ssl"
 # `path` is the name of the package for `import package`
 path = name.lower().replace("-", "_").replace(" ", "_")
 # Your version number should follow https://python.org/dev/peps/pep-0440 and
 # https://semver.org
 version = "0.1.dev0"
-author = "Author Name"
-author_email = ""
-description = ""  # One-liner
+author = "Shakhboz Abdulazizov"
+author_email = "sh873370@dal.ca"
+description = "Different SSL methods trained on BenthicNet data"  # One-liner
 url = ""  # your project homepage
 license = "MIT"  # See https://choosealicense.com
